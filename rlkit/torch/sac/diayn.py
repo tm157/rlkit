168bdce74eaa2fd91e3d148bdedbf24a8cf6cbee
Tanya Marwah <tanya.mrwh@gmail.com>
2018-04-07 20:28:09 -0400

    minor changes

---
+++ rlkit/torch/sac/diayn.py
@@ -0,0 +1,71 @@
+from collections import OrderedDict
+
+import numpy as np
+import torch.optim as optim
+from torch import nn as nn
+
+import rlkit.torch.pytorch_util as ptu
+from rlkit.core.eval_util import create_stats_ordered_dict
+from rlkit.torch.torch_rl_algorithm import TorchRLAlgorithm
+from rlkit.torch.sac.policies import MakeDeterministic
+
+
+class SoftActorCritic(TorchRLAlgorithm):
+    def __init__(
+            self,
+            env,
+            policy,
+            qf,
+            vf,
+
+            policy_lr=1e-3,
+            qf_lr=1e-3,
+            vf_lr=1e-3,
+            policy_mean_reg_weight=1e-3,
+            policy_std_reg_weight=1e-3,
+            policy_pre_activation_weight=0.,
+            optimizer_class=optim.Adam,
+
+            soft_target_tau=1e-2,
+            plotter=None,
+            render_eval_paths=False,
+            eval_deterministic=True,
+            **kwargs
+    ):
+        if eval_deterministic:
+            eval_policy = MakeDeterministic(policy)
+        else:
+            eval_policy = policy
+        super().__init__(
+            env=env,
+            exploration_policy=policy,
+            eval_policy=eval_policy,
+            **kwargs
+        )
+        self.policy = policy
+        self.qf = qf
+        self.vf = vf
+        self.soft_target_tau = soft_target_tau
+        self.policy_mean_reg_weight = policy_mean_reg_weight
+        self.policy_std_reg_weight = policy_std_reg_weight
+        self.policy_pre_activation_weight = policy_pre_activation_weight
+        self.plotter = plotter
+        self.render_eval_paths = render_eval_paths
+
+        self.target_vf = vf.copy()
+        self.qf_criterion = nn.MSELoss()
+        self.vf_criterion = nn.MSELoss()
+        self.eval_statistics = None
+
+        self.policy_optimizer = optimizer_class(
+            self.policy.parameters(),
+            lr=policy_lr,
+        )
+        self.qf_optimizer = optimizer_class(
+            self.qf.parameters(),
+            lr=qf_lr,
+        )
+        self.vf_optimizer = optimizer_class(
+            self.vf.parameters(),
+            lr=vf_lr,
+        )
